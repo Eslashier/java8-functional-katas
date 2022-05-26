8b461978df6297ac21a642eb74e89b56721bb0cd
Eslashier <anfelov@gmail.com>
2022-05-25 23:51:49 -0500

    kata 10 solved

--- src/main/java/Main2.java
+++ src/main/java/Main2.java
@@ -2,7 +2,7 @@ import katas.*;
 
 public class Main2 {
     public static void main(String[] args) {
-        System.out.println(Kata9.execute());
+        System.out.println(Kata10.execute());
 
     }
 }
